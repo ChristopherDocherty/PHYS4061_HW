d7f077b208727c3c9b8a5c673f99069f3e939264
pd-9999 <51860817+pd-9999@users.noreply.github.com>
2019-10-11 16:41:11 +0800

    Working matMult and tr functions

--- HW2/fadeev_Leverrier.cpp
+++ HW2/fadeev_Leverrier.cpp
@@ -13,34 +13,9 @@ using std::cout; using std::endl;
 */
 
 
-
-
-int main(){
-
-
-    const size_t n = 4;
-    double matA[n][n] = { 
-        {5,2,4,6},
-        {5,8,7,2},
-        {6,4,8,5},
-        {5,4,1,3}
-    };
-
-    
-
-      
-    double c[n];
-    double d[n][n];
-
-    double s[n][n][n];
-
-
-
-
-
-
-    return 0;
-}
+void fl(double s[4][4][4], double matA[4][4], double c[], size_t);
+void matMult(double z[4][4], double x[4][4], double y[4][4], size_t);
+double tr(double x[4][4], size_t n);
 
 
 
@@ -48,10 +23,10 @@ int main(){
 void fl(double s[4][4][4], double matA[4][4], double c[], size_t n){   
 
     
-    for(int i=0; i != n; ++i)
+    for(int i=0; i < n; ++i)
         s[0][i][i] = 1;
 
-    for(int i = 1; i != n; ++i){
+    for(int i = 1; i < n; ++i){
        matMult(s[i],matA,s[i-1],n);
        c[n-i] = -tr(s[i],n)/i;
 
@@ -68,19 +43,70 @@ void fl(double s[4][4][4], double matA[4][4], double c[], size_t n){
 
 void matMult(double z[4][4], double x[4][4], double y[4][4], size_t n){
 
-    for (int i = 0; i != n; i++)
-        for(int j=0; j != n; j++)
-            for(int k=0; k != n; k++)
+    //Initialise the matrix to get rid of random values
+    for (int i = 0; i < n; i++)
+        for(int j=0; j < n; j++)
+            z[i][j] = 0;
+
+    for (int i = 0; i < n; i++)
+        for(int j=0; j < n; j++)
+            for(int k=0; k < n; k++)
             z[i][j] += x[i][k] * y[k][j];
+            cout <<"blah"<<endl;
 }
 
 
 
 double tr(double x[4][4], size_t n){
     double sum = 0;
-    for(int i = 0; i!= n; i++)
+    for(int i = 0; i < n; i++)
         sum += x[i][i];
+    return sum;
 }
+
+
+
+
+int main(){
+
+
+    const size_t n = 4;
+    double matA[n][n] = { 
+        {5,2,4,6},
+        {5,8,7,2},
+        {6,4,8,5},
+        {5,4,1,3}
+    };
+
+    
+    double d[n][n];
+    
+    for(int i=0; i <n; ++i){
+        for(int j=0; j < n; ++j)
+            cout << d[i][j];
+        cout<<endl;
+    }
+
+    double c[n];
+
+    double s[n][n][n];
+    /*
+    fl(s,matA,c,n);
+
+    for(int i=0; i <n; ++i)
+        for(int j=0; j < n; ++j)
+            d[i][j] = -s[n-1][i][j]/c[0];
+    
+
+
+
+
+*/
+    return 0;
+}
+
+
+
 
 
 
